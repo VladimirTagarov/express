3d69b33225f9b15e27cf09f6a629dcb00cbfe2e2
VladimirTagarov <tagarovl@mail.ru>
2024-04-09 23:05:22 +0700

    Ошибки в синтаксисе

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -11,46 +11,50 @@ const getBook = (request, response) => {
   const { book_id } = request.params;
   // response.status(200);
   // response.send(`User with id: ${user_id}`);
-  return Book.findById(book_id).then((book) => {
-    response
-      .status(201)
-      .send(book)
-      .catch((error) => response.status(500).send(error.message));
-  });
+  return Book.findById(book_id)
+    .then((book) => {
+      response.status(201).send(book);
+    })
+    .catch((error) => {
+      response.status(500).send(error.message);
+    });
 };
 
 const createBook = (request, response) => {
   //Create new user
   // response.status(201);
   // response.send(request.body);
-  return Book.create({ ...request.body }).then((book) => {
-    response
-      .status(201)
-      .send(book)
-      .catch((error) => response.status(500).send(error.message));
-  });
+  return Book.create({ ...request.body })
+    .then((book) => {
+      response.status(201).send(book);
+    })
+    .catch((error) => {
+      response.status(500).send(error.message);
+    });
 };
 
 const updateBook = (request, response) => {
   //Update book
   const { book_id } = request.params;
-  return Book.findByIdAndUpdate(book_id, { ...request.body }).then((book) => {
-    response
-      .status(201)
-      .send(book)
-      .catch((error) => response.status(500).send(error.message));
-  });
+  return Book.findByIdAndUpdate(book_id, { ...request.body })
+    .then((book) => {
+      response.status(201).send(book);
+    })
+    .catch((error) => {
+      response.status(500).send(error.message);
+    });
 };
 
 const deleteBook = (request, response) => {
   //Delete book
   const { book_id } = request.params;
-  return Book.findByIdAndDelete(book_id, { ...request.body }).then((book) => {
-    response
-      .status(201)
-      .send(book)
-      .catch((error) => response.status(500).send(error.message));
-  });
+  return Book.findByIdAndDelete(book_id, { ...request.body })
+    .then((book) => {
+      response.status(201).send(book);
+    })
+    .catch((error) => {
+      response.status(500).send(error.message);
+    });
 };
 
 module.exports = {
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -11,46 +11,50 @@ const getUser = (request, response) => {
   const { user_id } = request.params;
   // response.status(200);
   // response.send(`User with id: ${user_id}`);
-  return User.findById(user_id).then((user) => {
-    response
-      .status(201)
-      .send(user)
-      .catch((error) => response.status(500).send(error.message));
-  });
+  return User.findById(user_id)
+    .then((user) => {
+      response.status(201).send(user);
+    })
+    .catch((e) => {
+      response.status(500).send(e.message);
+    });
 };
 
 const createUser = (request, response) => {
   //Create new user
   // response.status(201);
   // response.send(request.body);
-  return User.create({ ...request.body }).then((user) => {
-    response
-      .status(201)
-      .send(user)
-      .catch((error) => response.status(500).send(error.message));
-  });
+  return User.create({ ...request.body })
+    .then((user) => {
+      response.status(201).send(user);
+    })
+    .catch((e) => {
+      response.status(500).send(e.message);
+    });
 };
 
 const updateUser = (request, response) => {
   //Update user
   const { user_id } = request.params;
-  return User.findByIdAndUpdate(user_id, { ...request.body }).then((user) => {
-    response
-      .status(201)
-      .send(user)
-      .catch((error) => response.status(500).send(error.message));
-  });
+  return User.findByIdAndUpdate(user_id, { ...request.body })
+    .then((user) => {
+      response.status(201).send(user);
+    })
+    .catch((e) => {
+      response.status(500).send(e.message);
+    });
 };
 
 const deleteUser = (request, response) => {
   //Delete user
   const { user_id } = request.params;
-  return User.findByIdAndDelete(user_id, { ...request.body }).then((user) => {
-    response
-      .status(201)
-      .send(user)
-      .catch((error) => response.status(500).send(error.message));
-  });
+  return User.findByIdAndDelete(user_id, { ...request.body })
+    .then((user) => {
+      response.status(201).send(user);
+    })
+    .catch((e) => {
+      response.status(500).send(e.message);
+    });
 };
 
 module.exports = {
